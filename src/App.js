28338ae748e037d48586e8be3b9eaa44decaf3c6
Andrei-J-F-P <an.ja.fe.po@gmail.com>
2020-09-29 15:37:36 +0800

    edited app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed from the Feature branch and merged
+          Changed from the Feature branch and merged twice
         </p>
         <a
           className="App-link"
